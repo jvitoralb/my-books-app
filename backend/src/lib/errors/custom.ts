1d8bce283859c1eca4cf9f70644799f8a7fceff4
jvitoralb <jvvalb@gmail.com>
2023-08-13 12:27:56 -0300

    upload: custom errors

---
+++ backend/src/lib/errors/custom.ts
@@ -0,0 +1,20 @@
+class AppError extends Error {
+    name: string;
+    statusCode: number;
+
+    constructor(message: string, name: string, statusCode: number) {
+        super(message);
+        this.name = name;
+        this.statusCode = statusCode;
+    }
+}
+
+class BadRequestError extends AppError {
+    constructor(message: string, name = 'BAD REQUEST', statusCode = 400) {
+        super(message, name, statusCode);
+    }
+}
+
+export {
+    BadRequestError
+};
