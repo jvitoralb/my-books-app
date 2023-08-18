d7a3bc6d6d8cbbb9139c8c5afc1a0faf4f72032a
jvitoralb <jvvalb@gmail.com>
2023-08-17 22:19:15 -0300

    feature: add description field to custom errors

--- backend/src/lib/errors/custom.ts
+++ backend/src/lib/errors/custom.ts
@@ -1,32 +1,33 @@
 class AppError extends Error {
     name: string;
     statusCode: number;
+    description: string;
 
-    constructor(message: string, name: string, statusCode: number) {
+    constructor(message: string, name: string, statusCode: number, description: string) {
         super(message);
         this.name = name;
         this.statusCode = statusCode;
+        this.description = description;
     }
 }
 
 class AuthenticationError extends AppError {
-    constructor(message: string, name: string, statusCode: number) {
-        super(message, name, statusCode);
+    constructor(message: string, name: string, statusCode: number, description = '') {
+        super(message, name, statusCode, description);
     }
 }
 
 class BadRequestError extends AppError {
-    constructor(message: string, name = 'BAD REQUEST', statusCode = 400) {
-        super(message, name, statusCode);
+    constructor(message: string, name = 'BAD REQUEST', statusCode = 400, description = '') {
+        super(message, name, statusCode, description);
     }
 }
 
 class ServerError extends AppError {
-    constructor(message = 'Internal Server Error', name = 'SERVER ERROR', statusCode = 500) {
-        super(message, name, statusCode);
+    constructor(message = 'Internal Server Error', name = 'SERVER ERROR', statusCode = 500, description = '') {
+        super(message, name, statusCode, description);
     }
 }
-
 export {
     BadRequestError,
     AuthenticationError,
