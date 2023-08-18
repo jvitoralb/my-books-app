3fe0b88a19ee63c40ef063c87742434d011dee91
jvitoralb <jvvalb@gmail.com>
2023-08-18 00:25:20 -0300

    feature: add update routes and controllers

--- backend/src/components/user/api/controllers.ts
+++ backend/src/components/user/api/controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import UserService from '../service/services';
+import AuthToken from '../../../lib/auth/jwt';
 
 interface UserInputs {
     id?: string;
@@ -9,7 +10,9 @@ interface UserInputs {
 }
 
 interface Controller {
-    create(req: Request, res: Response, next: NextFunction): Promise<void>
+    create(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updateEmail(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updatePswd(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
 class UserController implements Controller {
@@ -31,6 +34,33 @@ class UserController implements Controller {
             const newUser = await this.service.registerUser(password);
 
             res.status(201).json(newUser);
+        } catch(err) {
+            next(err);
+        }
+    }
+    updateEmail = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const bearerToken = req.get('Authorization');
+            const payload = new AuthToken().decode(bearerToken!);
+            const newEmail: string = req.body.new_email;
+
+            this.service.setUserNameAndEmail = { name: '', email: newEmail };
+            const updatedUser = await this.service.changeEmail(payload.sub);
+
+            res.status(200).json(updatedUser);
+        } catch(err) {
+            next(err);
+        }
+    }
+    updatePswd = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const bearerToken = req.get('Authorization');
+            const payload = new AuthToken().decode(bearerToken!);
+            const newPswd: string = req.body.new_password;
+
+            await this.service.changePassword({ id: payload.sub, password: newPswd });
+
+            res.status(204).json();
         } catch(err) {
             next(err);
         }
--- backend/src/components/user/api/routes.ts
+++ backend/src/components/user/api/routes.ts
@@ -6,5 +6,7 @@ import { CreateUserValidation } from './middlewares';
 const userRouter = Router();
 
 userRouter.post('/users', new CreateUserValidation().checkAllInputs, new UserController().create);
+userRouter.put('/users/email', new UserController().updateEmail);
+userRouter.put('/users/password', new UserController().updatePswd);
 
 export default userRouter;
