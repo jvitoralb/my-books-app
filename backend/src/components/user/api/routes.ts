621884bb796edaa31af3d7bbffc8c6ee22347f27
jvitoralb <jvvalb@gmail.com>
2023-08-18 16:21:46 -0300

    feature: new routes for accessing user data

--- backend/src/components/user/api/routes.ts
+++ backend/src/components/user/api/routes.ts
@@ -5,7 +5,9 @@ import { CreateUserValidation } from './middlewares';
 
 const userRouter = Router();
 
+userRouter.get('/users', new UserController().read);
 userRouter.delete('/users', new UserController().delete);
+userRouter.post('/users/login', new UserController().readCredentials);
 userRouter.post('/users/register', new CreateUserValidation().checkAllInputs, new UserController().create);
 userRouter.put('/users/email', new UserController().updateEmail);
 userRouter.put('/users/password', new UserController().updatePswd);
