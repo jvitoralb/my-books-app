c3a11bc072800fd4a93a87ad3903b9592f4366d6
jvitoralb <jvvalb@gmail.com>
2023-08-19 23:08:34 -0300

    update: handler to better answer the server errors

--- backend/src/lib/errors/handler.ts
+++ backend/src/lib/errors/handler.ts
@@ -1,7 +1,14 @@
 import { ErrorRequestHandler } from 'express';
+import { AppError, ServerError } from './custom';
 
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.log(err);
+
+    if (!(err instanceof AppError)) {
+        err = new ServerError();
+    }
+
     res.status(err.statusCode).json({
         error: err.message
     });
