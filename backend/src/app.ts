79aa7defa866a54bc6248c24091d7f33b88a6e29
jvitoralb <jvvalb@gmail.com>
2023-08-25 16:01:31 -0300

    feature: add bookRouter to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import userRouter from './components/user/api/routes';
+import bookRouter from './components/book/api/routes';
 import errorHandler from './lib/errors/handler';
 
 
@@ -8,6 +9,7 @@ const app: Express = express();
 app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/books', bookRouter);
 
 app.use(errorHandler);
 
