a69fec7bb69f46c20baa704934977465f23ce4a5
jvitoralb <jvvalb@gmail.com>
2023-08-26 23:10:16 -0300

    update: added all routes

--- backend/src/components/book/api/routes.ts
+++ backend/src/components/book/api/routes.ts
@@ -5,6 +5,10 @@ import BookController from './controllers';
 const bookRouter = Router();
 
 bookRouter.post('/', new BookController().create);
+bookRouter.get('/', new BookController().read);
+bookRouter.delete('/:id', new BookController().delete);
+bookRouter.put('/:id/info', new BookController().updateInfo);
+bookRouter.put('/:id/section', new BookController().updateSection);
 
 
 export default bookRouter;
