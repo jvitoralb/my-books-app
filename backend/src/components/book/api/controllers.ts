c2d48e9ffbce4d76e1e7efc6d4bff003d8f92319
jvitoralb <jvvalb@gmail.com>
2023-08-26 23:10:44 -0300

    update: add all necessary methods

--- backend/src/components/book/api/controllers.ts
+++ backend/src/components/book/api/controllers.ts
@@ -10,6 +10,10 @@ interface BookInputs {
 
 interface Controller {
     create(req: Request, res: Response, next: NextFunction): Promise<void>;
+    read(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updateInfo(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updateSection(req: Request, res: Response, next: NextFunction): Promise<void>;
+    delete(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
 class BookController implements Controller {
@@ -25,6 +29,53 @@ class BookController implements Controller {
         const newBook = await this.service.saveBook(bookInputs);
 
         res.status(201).json(newBook);
+    }
+    read = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const books = await this.service.searchBooks();
+    
+            res.status(200).json(books);
+        } catch(err) {
+            next(err);
+        }
+    }
+    updateInfo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { id } = req.params;
+            const bookData: Book = {
+                ...req.body,
+                id
+            };
+
+            await this.service.changeBookInfo(bookData);
+
+            res.status(204).json();
+        } catch(err) {
+            next(err);
+        }
+    }
+    updateSection = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { id } = req.params;
+            const { section }: { section: string } = req.body;
+
+            await this.service.changeBookSection(id, section);
+
+            res.status(204).json();
+        } catch(err) {
+            next(err);
+        }
+    }
+    delete = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { id } = req.params;
+
+            await this.service.destroyBook(id);
+
+            res.status(204).json();
+        } catch(err) {
+            next(err);
+        }
     }
 }
 
