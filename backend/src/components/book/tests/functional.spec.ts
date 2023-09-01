52561e735bb88b5bf80057aad482b804fc9f1753
jvitoralb <jvvalb@gmail.com>
2023-09-01 12:47:44 -0300

    fix: read array of books test

--- backend/src/components/book/tests/functional.spec.ts
+++ backend/src/components/book/tests/functional.spec.ts
@@ -25,7 +25,10 @@ describe('Book Component Crud Tests', () => {
         .set('Authorization', userToken.mock.results[0].value)
         .set('Accept', 'application/json');
 
-        serverResponse({ body: res.body, statusCode: res.statusCode });
+        serverResponse({
+            body: res.body,
+            statusCode: res.statusCode
+        });
 
         expect(serverResponse).toHaveBeenCalledWith(
             expect.objectContaining({
@@ -38,12 +41,39 @@ describe('Book Component Crud Tests', () => {
         );
     });
 
+    test('should answer with all created books', async () => {
+        const res = await request(app)
+        .get('/api/v1/books')
+        .set('Authorization', userToken.mock.results[0].value)
+        .set('Accept', 'application/json');
+
+        serverResponse({
+            body: res.body,
+            statusCode: res.statusCode
+        });
+
+        expect(serverResponse).toHaveBeenCalledWith(
+            expect.objectContaining({
+                body: expect.arrayContaining([
+                    expect.objectContaining({
+                        id: expect.any(String),
+                        user_id: expect.any(String),
+                        title: expect.any(String),
+                        created_at: expect.any(String)
+                    })
+                ]),
+                statusCode: 200
+            })
+        );
+    });
+
     test('should answer with 204 when successfully updates info', async () => {
         const res = await request(app)
-        .put(`/api/v1/books/${serverResponse.mock.results[0].value.body.id as string}/info`)
+        .put(`/api/v1/books/${serverResponse.mock.results[0].value.body.id}/info`)
         .send({
             title: 'A volta dos que não foram',
-            author: 'ninguém'
+            author: 'ninguém',
+            about: null
         })
         .set('Authorization', userToken.mock.results[0].value)
         .set('Accept', 'application/json');
@@ -53,42 +83,17 @@ describe('Book Component Crud Tests', () => {
 
     test('should answer with 204 when successfully updates section', async () => {
         const res = await request(app)
-        .put(`/api/v1/books/${serverResponse.mock.results[0].value.body.id as string}/section`)
-        .send({ section: 'section updated' })
+        .put(`/api/v1/books/${serverResponse.mock.results[0].value.body.id}/section`)
+        .send({ section: 'section update' })
         .set('Authorization', userToken.mock.results[0].value)
         .set('Accept', 'application/json');
 
         expect(res.statusCode).toBe(204);
     });
 
-    test('should answer with all created books', async () => {
-        const res = await request(app)
-        .get('/api/v1/books')
-        .set('Authorization', userToken.mock.results[0].value)
-        .set('Accept', 'application/json');
-
-        serverResponse({ body: res.body, statusCode: 200 });
-
-        expect(serverResponse).toHaveBeenCalledWith(
-            expect.objectContaining({
-                body: [
-                    expect.objectContaining({
-                        id: expect.any(String),
-                        user_id: expect.any(String),
-                        title: expect.any(String),
-                        author: expect.any(String),
-                        about: expect.any(String),
-                        section: expect.any(String),
-                    })
-                ],
-                statusCode: 200
-            })
-        );
-    });
-
     test('should be able to delete a book successfully and return 204', async () => {
         const res = await request(app)
-        .delete(`/api/v1/books/${serverResponse.mock.results[0].value.body.id as string}`)
+        .delete(`/api/v1/books/${serverResponse.mock.results[0].value.body.id}`)
         .set('Authorization', userToken.mock.results[0].value)
         .set('Accept', 'application/json');
 
