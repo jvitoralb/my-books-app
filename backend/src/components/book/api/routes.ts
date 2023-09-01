bd3e6da7d32809e9b9b1256bfc0d38b0984719aa
jvitoralb <jvvalb@gmail.com>
2023-09-01 11:12:49 -0300

    update: add BookMiddleware

--- backend/src/components/book/api/routes.ts
+++ backend/src/components/book/api/routes.ts
@@ -7,8 +7,8 @@ const bookRouter = Router();
 
 bookRouter.post('/', new BookMiddleware().validateCreate, new BookController().create);
 bookRouter.get('/', new BookController().read);
-bookRouter.delete('/:id', new BookMiddleware().validateDelete, new BookController().delete);
+bookRouter.delete('/:id', new BookMiddleware().validateDelete , new BookController().delete);
 bookRouter.put('/:id/info', new BookMiddleware().validateUpdateInfo, new BookController().updateInfo);
-bookRouter.put('/:id/section', new BookController().updateSection);
+bookRouter.put('/:id/section', new BookMiddleware().validateUpdateSection, new BookController().updateSection);
 
 export default bookRouter;
