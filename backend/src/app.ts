bb3a9d294dda9fc402aeb139372a891f1177354f
jvitoralb <jvvalb@gmail.com>
2023-09-06 22:08:22 -0300

    update: enable cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,19 @@ import userRouter from './components/user/api/routes';
 import bookRouter from './components/book/api/routes';
 import errorHandler from './lib/errors/handler';
 import notFoundHandler from './lib/404/handler';
+import cors from 'cors';
 
 
 const app: Express = express();
 
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin']
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
