aef6190d99e4eb21d25d84f8906d017bd48f2ddc
jvitoralb <jvvalb@gmail.com>
2023-09-13 22:19:45 -0300

    upload: ts types

---
+++ frontend/src/types.ts
@@ -0,0 +1,14 @@
+export type LoginCredentials = {
+    email: string;
+    password: string;
+}
+
+export type UserAuth = {
+    token: string;
+    expires: string;
+}
+
+export type User = {
+    name: string;
+    email: string;
+}
