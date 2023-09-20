6a59c5e6a652b27edf7e2557cf39ec75f661efae
jvitoralb <jvvalb@gmail.com>
2023-09-20 15:58:16 -0300

    update: move routes to a separated file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import {
   QueryClientProvider,
   QueryClient
 } from '@tanstack/react-query';
-import App from './App.tsx';
-import Home, { homeLoader } from './pages/Home.tsx';
-import Welcome from './pages/Welcome.tsx';
-import LogIn, { loginLoader } from './pages/Login.tsx';
-import SignUp from './pages/Signup.tsx';
+import browserAppRouter from './routes';
 import './styles/index.css';
 
-
-const browserAppRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index element={<Home />} loader={homeLoader} />
-      <Route path='/welcome' element={<Welcome />} />
-      <Route path='/login' element={<LogIn />} loader={loginLoader}/>
-      <Route path='/signup' element={<SignUp />} />
-    </Route>
-  )
-);
 
 const queryClient = new QueryClient();
 
