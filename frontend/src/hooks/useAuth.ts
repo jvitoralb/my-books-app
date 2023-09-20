5639c4a24a684d0e5e3aa49f04052f884845cb3c
jvitoralb <jvvalb@gmail.com>
2023-09-19 23:36:49 -0300

    update: hook params + refactor code

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -5,8 +5,6 @@ import { getAuthToken, setAuthToken } from '../utils/auth';
 type AuthHookOptions = {
     data?: UserAuth;
     operation: 'SET' | 'GET';
-    setter?: Function;
-    getter?: Function;
 }
 
 type AuthState = {
@@ -14,31 +12,31 @@ type AuthState = {
     token: string;
 }
 
-const useAuth = ({ data, operation, setter, getter }: AuthHookOptions): AuthState => {
-    if (!setter) setter = setAuthToken;
-    if (!getter) getter = getAuthToken;
-
+const useAuth = ({ data, operation }: AuthHookOptions): AuthState => {
     const [ auth, setAuth ] = useState<AuthState>({
         isAuth: false,
         token: ''
     });
 
-    if (operation === 'SET' && !auth.isAuth && data?.token) {
-        setter(data.token);
-
-        setAuth((prevAuth) => ({
-            ...prevAuth,
-            isAuth: true
-        }));
+    if (operation === 'SET') {
+        if (!auth.isAuth && data) {
+            setAuthToken(data);
+        
+            setAuth((prev) => ({
+                ...prev,
+                isAuth: true
+            }));
+        }
+    } else if (operation === 'GET') {
+        let authData = getAuthToken();
+        
+        if (auth.token === '' && authData.token !== '') {
+            setAuth((prevAuth) => ({
+                ...prevAuth,
+                token: authData.token
+            }));
+        }
     }
-    // else if (operation === 'GET' && auth.token === '') {
-    //     let authToken = getter();
-
-    //     setAuth((prevAuth) => ({
-    //         ...prevAuth,
-    //         token: authToken
-    //     }));
-    // }
 
     return {
         isAuth: auth.isAuth,
