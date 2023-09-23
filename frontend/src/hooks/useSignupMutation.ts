23c9565ab961a2edac53907fe835bd71c824436c
jvitoralb <jvvalb@gmail.com>
2023-09-22 21:50:02 -0300

    update: add error stats

--- frontend/src/hooks/useSignupMutation.ts
+++ frontend/src/hooks/useSignupMutation.ts
@@ -1,15 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
 import { createUser } from '../api/api';
+import { ServerErrorMessage, SignupUserData, UserAuth } from '../types';
+import { AxiosError } from 'axios';
 
 
 const useSignupMutation = () => {
-    const { data, mutate } = useMutation({
+    const { data, mutate, isError, error } = useMutation<UserAuth, AxiosError<ServerErrorMessage>, SignupUserData>({
         mutationFn: createUser
     });
 
     return {
         data,
-        mutate
+        mutate,
+        isError,
+        error
     }
 }
 
