93efc7c1576283c50db8a64d4c8bf15863121fed
jvitoralb <jvvalb@gmail.com>
2023-09-30 02:06:08 -0300

    update: implemented a handler for error messages

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,19 +1,56 @@
-import { ChangeEvent, FormEvent } from 'react';
-import { LoginProps } from '../types';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { FieldsArray, LoginProps } from '../types';
 import { Button, FormControl, FormHelperText, FormLabel, Heading, Input } from '@chakra-ui/react';
 
 
-function LogIn({ refetch, error, isError, validCredentials, setCredentials }: LoginProps) {
+function LogIn({ refetch, error, isError, setCredentials, isValid, fields }: LoginProps) {
+  const [ warnings, setWarnings ] = useState({
+    display: false,
+    email: '',
+    password: ''
+  });
+
+  const handleWarnings = (warnFields: FieldsArray | null) => {
+    let emailMsg = '';
+    let passwordMsg = '';
+
+    if (warnFields === null) {
+      setWarnings({
+        display: false,
+        email: emailMsg,
+        password: passwordMsg
+      });
+      return;
+    }
+
+    if (warnFields[0] !== null) {
+      if (isValid === false) emailMsg = 'Invalid email!';
+      if (isError === true) emailMsg = 'Email does not exists';
+    }
+    if (warnFields[1] !== null) {
+      if (isValid === false) passwordMsg = 'Invalid password';
+      if (isError === true) passwordMsg = 'Wrong password';
+    }
+
+    setWarnings({
+      display: true,
+      email: emailMsg,
+      password: passwordMsg
+    });
+  }
+
   const handleInputsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (warnings.display) handleWarnings(null);
     setCredentials(e.target.name, e.target.value);
   }
-  
+
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (validCredentials) {
+    
+    if (isValid) {
       refetch();
     } else {
-      // show invalid credentials
+      handleWarnings(fields);
     }
   }
   
@@ -22,13 +59,17 @@ function LogIn({ refetch, error, isError, validCredentials, setCredentials }: Lo
 
     if (errorRes?.status === 400) {
       let dataError = errorRes?.data.error;
+      let queryWarnFields: FieldsArray = [ null, null ];
 
-      if (dataError === 'User does not exists') {
-        dataError = dataError.replace('User', 'Email');
+      if (dataError === 'User does not exists') queryWarnFields[0] = 'email';
+      if (dataError === 'Invalid password') queryWarnFields[1] = 'password';
+
+      if (warnings.display === false) {
+        handleWarnings(queryWarnFields);
       }
-      return `${dataError}!`;
+      return undefined;
     }
-    return 'Something went wrong. Please, try again later!';
+    return <p className="error-message">Something went wrong. Please, try again later!</p>;
   }
 
   return (
@@ -37,23 +78,29 @@ function LogIn({ refetch, error, isError, validCredentials, setCredentials }: Lo
         <Heading as="h4" size="md" mb="3">Login</Heading>
 
         <form id="login-form" onSubmit={handleFormSubmit} className="flex-center-col">
-          <FormControl m="1">
+          <FormControl m="1" isInvalid={warnings.display && warnings.email !== ''}>
             <FormLabel htmlFor="email">Email address</FormLabel>
             <Input id="email" name="email" type="email" placeholder="my-mail@books.app" onChange={handleInputsChange} required />
-            <FormHelperText mt="0.5">We'll never share your email.</FormHelperText>
+            {
+              warnings.display && warnings.email ? 
+              <FormHelperText mt="0.5">{warnings.email}</FormHelperText> : 
+              <FormHelperText mt="0.5">We'll never share your email.</FormHelperText>
+            }
           </FormControl>
 
-          <FormControl m="1">
+          <FormControl m="1" isInvalid={warnings.display && warnings.password !== ''}>
             <FormLabel htmlFor="password">Password</FormLabel>
             <Input id="password" name="password" type="password" onChange={handleInputsChange} required />
-            <FormHelperText mt="0.5">We'll never ask for your password.</FormHelperText>
+            {
+              warnings.display && warnings.password ? 
+              <FormHelperText mt="0.5">{warnings.password}</FormHelperText> :
+              <FormHelperText mt="0.5">We'll never ask for your password.</FormHelperText>
+            }
           </FormControl>
 
           <Button id="login-submit" type="submit" m="2">Login</Button>
         </form>
-        {
-          isError && <p className="error-message">{handleQueryError()}</p>
-        }
+        { isError && handleQueryError() }
       </section>
     </main>
   );
