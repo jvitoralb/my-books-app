f32d21549a4b95d037069a356ba5c27a11632084
jvitoralb <jvvalb@gmail.com>
2023-09-30 02:03:38 -0300

    update: login validation

--- frontend/src/hooks/useLoginCredentials.ts
+++ frontend/src/hooks/useLoginCredentials.ts
@@ -1,12 +1,44 @@
-import { useState } from 'react';
-import { LoginCredentials } from '../types';
+import { useEffect, useState } from 'react';
+import { LoginCredentials, FieldsArray } from '../types';
 
+type LoginValidation = {
+  isValid: boolean;
+  fields: FieldsArray;
+}
 
 const useLoginCredentials = () => {
   const [ loginCredentials, setLoginCredentials ] = useState<LoginCredentials>({
     email: '',//yo.admin.jv@library.app
     password: ''//yo.admin.jv-strongpswd123
   });
+
+  const [ validation, setValidation ] = useState<LoginValidation>({
+    isValid: false,
+    fields: [null, null]
+  });
+
+  useEffect(() => validate(), [loginCredentials]);
+
+  const validate = () => {
+    let msgsMock: FieldsArray = [null, null];
+
+    if (loginCredentials.email.match(/^\S+[@]\S+[.]\S+$/) === null) {
+      msgsMock[0] = 'email';
+    } else {
+      msgsMock[0] = null;
+    }
+
+    if (loginCredentials.password.length < 8) {
+      msgsMock[1] = 'password';
+    } else {
+      msgsMock[1] = null;
+    }
+
+    setValidation({
+      isValid: (msgsMock[0] === null && msgsMock[1] === null) ? true : false,
+      fields: msgsMock
+    });
+  }
 
   const setCredentials = (inputName: string, inputValue: string) => {
     setLoginCredentials((prevCred) => ({
@@ -15,20 +47,11 @@ const useLoginCredentials = () => {
     }));
   }
 
-  const validateCredentials = (): boolean => {
-    if (loginCredentials.email.match(/^\S+[@]\S+[.]\S+$/) === null) {
-      return false;
-    }
-    if (loginCredentials.password.length < 8) {
-      return false;
-    }
-    return true;
-  }
-
   return {
     credentials: loginCredentials,
     setCredentials: setCredentials,
-    validCredentials: validateCredentials()
+    isValid: validation.isValid,
+    fields: validation.fields
   }
 }
 
