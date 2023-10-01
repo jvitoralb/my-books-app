832721ff48be7cfd0c0297b679937fd843504f3f
jvitoralb <jvvalb@gmail.com>
2023-09-30 22:14:51 -0300

    update: refactor code

--- frontend/src/hooks/useWarning.ts
+++ frontend/src/hooks/useWarning.ts
@@ -1,41 +1,45 @@
-import { useState } from "react";
-import { FieldsArray } from "../types";
+import { useState } from 'react';
+import { FieldsArray } from '../types';
 
+type Warnings = {
+    display: boolean;
+    email: string;
+    password: string;
+}
 
 const useWarnings = (isValid: boolean, isError: boolean) => {
-    const [warnings, setWarnings] = useState({
+    const [warnings, setWarnings] = useState<Warnings>({
         display: false,
         email: '',
         password: ''
     });
 
-    const handleWarnings = (warnFields: FieldsArray | null) => {
-        let emailMsg = '';
-        let passwordMsg = '';
-
-        if (warnFields === null) {
-            setWarnings({
-                display: false,
-                email: emailMsg,
-                password: passwordMsg
-            });
-            return;
-        }
-
-        if (warnFields[0] !== null) {
-            if (isValid === false) emailMsg = 'Invalid email!';
-            if (isError === true) emailMsg = 'Email does not exists';
-        }
-        if (warnFields[1] !== null) {
-            if (isValid === false) passwordMsg = 'Invalid password';
-            if (isError === true) passwordMsg = 'Wrong password';
-        }
-
+    const set = (display: boolean, email: string, password: string) => {
         setWarnings({
-            display: true,
-            email: emailMsg,
-            password: passwordMsg
+            display,
+            email,
+            password
         });
+    }
+
+    const handleWarnings = (warnFields: FieldsArray | null) => {
+        if (warnFields !== null) {
+            let emailMsg = '';
+            let passwordMsg = '';
+
+            if (warnFields[0] !== null) {
+                if (isValid === false) emailMsg = 'Invalid email!';
+                if (isError === true) emailMsg = 'Email does not exists';
+            }
+            if (warnFields[1] !== null) {
+                if (isValid === false) passwordMsg = 'Invalid password';
+                if (isError === true) passwordMsg = 'Wrong password';
+            }
+
+            set(true, emailMsg, passwordMsg);
+        } else {
+            set(false, '', '');
+        }
     }
 
     return {
