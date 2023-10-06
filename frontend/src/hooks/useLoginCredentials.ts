9672cb3a0cdcbb56ecb19f298ec50be9dea4c537
jvitoralb <jvvalb@gmail.com>
2023-10-06 15:16:07 -0300

    fix: rm comments

--- frontend/src/hooks/useLoginCredentials.ts
+++ frontend/src/hooks/useLoginCredentials.ts
@@ -2,57 +2,57 @@ import { useEffect, useState } from 'react';
 import { LoginCredentials, FieldsArray } from '../types';
 
 type LoginValidation = {
-  isValid: boolean;
-  fields: FieldsArray;
+    isValid: boolean;
+    fields: FieldsArray;
 }
 
 const useLoginCredentials = () => {
-  const [ loginCredentials, setLoginCredentials ] = useState<LoginCredentials>({
-    email: '',//yo.admin.jv@library.app
-    password: ''//yo.admin.jv-strongpswd123
-  });
-
-  const [ validation, setValidation ] = useState<LoginValidation>({
-    isValid: false,
-    fields: [null, null]
-  });
-
-  useEffect(() => validate(), [loginCredentials]);
-
-  const validate = () => {
-    let msgsMock: FieldsArray = [null, null];
-
-    if (loginCredentials.email.match(/^\S+[@]\S+[.]\S+$/) === null) {
-      msgsMock[0] = 'email';
-    } else {
-      msgsMock[0] = null;
-    }
-
-    if (loginCredentials.password.length < 8) {
-      msgsMock[1] = 'password';
-    } else {
-      msgsMock[1] = null;
-    }
-
-    setValidation({
-      isValid: (msgsMock[0] === null && msgsMock[1] === null) ? true : false,
-      fields: msgsMock
+    const [ loginCredentials, setLoginCredentials ] = useState<LoginCredentials>({
+        email: '',
+        password: ''
     });
-  }
 
-  const setCredentials = (inputName: string, inputValue: string) => {
-    setLoginCredentials((prevCred) => ({
-      ...prevCred,
-      [inputName]: inputValue
-    }));
-  }
+    const [ validation, setValidation ] = useState<LoginValidation>({
+        isValid: false,
+        fields: [null, null]
+    });
 
-  return {
-    credentials: loginCredentials,
-    setCredentials: setCredentials,
-    isValid: validation.isValid,
-    fields: validation.fields
-  }
+    useEffect(() => validate(), [loginCredentials]);
+
+    const validate = () => {
+        let msgsMock: FieldsArray = [null, null];
+
+        if (loginCredentials.email.match(/^\S+[@]\S+[.]\S+$/) === null) {
+            msgsMock[0] = 'email';
+        } else {
+            msgsMock[0] = null;
+        }
+
+        if (loginCredentials.password.length < 8) {
+            msgsMock[1] = 'password';
+        } else {
+            msgsMock[1] = null;
+        }
+
+        setValidation({
+            isValid: (msgsMock[0] === null && msgsMock[1] === null) ? true : false,
+            fields: msgsMock
+        });
+    }
+
+    const setCredentials = (inputName: string, inputValue: string) => {
+        setLoginCredentials((prevCred) => ({
+            ...prevCred,
+            [inputName]: inputValue
+        }));
+    }
+
+    return {
+        credentials: loginCredentials,
+        setCredentials: setCredentials,
+        isValid: validation.isValid,
+        fields: validation.fields
+    }
 }
 
 export default useLoginCredentials;
