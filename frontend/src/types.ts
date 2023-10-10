adfa7a9a4de1434b2bea8601fc892a1ab0393010
jvitoralb <jvvalb@gmail.com>
2023-10-09 21:22:38 -0300

    update: add input control types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,38 +1,37 @@
-import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from '@tanstack/react-query';
+import { QueryObserverResult, RefetchOptions, RefetchQueryFilters, UseMutateFunction } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import { HTMLInputTypeAttribute } from 'react';
 
+/**
+ * General Types
+**/
+export type ServerErrorMessage = {
+    error: string
+}
+export type CustomAxiosError = AxiosError<ServerErrorMessage, any> | null
+export type FieldsArray = ['email' | null, 'password' | 'confirm_password' | null];
+export type UseQueryRefetch = <TPageData>(options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined) => Promise<QueryObserverResult<UserAuth, AxiosError<ServerErrorMessage, any>>>;
 
+/**
+ * Input Control Component related Types
+**/
+export type InputSubject = 'name' | 'email' | 'password' | 'confirm_password';
+export type InputControlProps = {
+    subject: InputSubject;
+    inputType?: HTMLInputTypeAttribute;
+    setFormState: (inputName: string, inputValue: string) => void;
+    displayWarning: boolean;
+    handleWarnings: (warnFields: FieldsArray | null) => void;
+    subjectWarning: string;
+}
+
+/**
+ * Login Component related Types
+**/
 export type LoginCredentials = {
     email: string;
     password: string;
 }
-
-export type UserAuth = {
-    token: string;
-    expires: string;
-}
-
-export type User = {
-    name: string;
-    email: string;
-}
-
-export type SignupUserData = {
-    name: string;
-    email: string;
-    password: string;
-}
-
-export type ServerErrorMessage = {
-    error: string
-}
-
-export type CustomAxiosError = AxiosError<ServerErrorMessage, any> | null
-
-export type FieldsArray = ['email' | null, 'password' | null];
-
-export type UseQueryRefetch = <TPageData>(options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined) => Promise<QueryObserverResult<UserAuth, AxiosError<ServerErrorMessage, any>>>;
-
 export type LoginProps = {
     refetch: <TPageData>(options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined) => Promise<QueryObserverResult<UserAuth, AxiosError<ServerErrorMessage, any>>>;
     isError: boolean;
@@ -41,7 +40,6 @@ export type LoginProps = {
     isValid: boolean;
     fields: FieldsArray;
 }
-
 export type LoginFormProps = {
     refetch: <TPageData>(options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined) => Promise<QueryObserverResult<UserAuth, AxiosError<ServerErrorMessage, any>>>;
     isError: boolean;
@@ -51,12 +49,48 @@ export type LoginFormProps = {
     fields: FieldsArray;
 }
 
-export type LoginInputSubject = 'email' | 'password';
+/**
+ * Signup Component related Types
+**/
+export type SignupUserData = {
+    name: string;
+    email: string;
+    password: string;
+}
+export type SignupData = {
+    name: string;
+    email: string;
+    password: string;
+    confirm_password: string;
+}
+export type SignupProps = {
+    mutate: UseMutateFunction<UserAuth, AxiosError<ServerErrorMessage, any>, SignupUserData, unknown>;
+    isError: boolean;
+    error: CustomAxiosError;
+    setSignupData: (name: string, value: string) => void;
+    signupData: SignupData;
+    isValid: boolean;
+    fields: FieldsArray;
+}
+export type SignupFormProps = {
+    mutate: UseMutateFunction<UserAuth, AxiosError<ServerErrorMessage, any>, SignupUserData, unknown>;
+    isError: boolean;
+    errorFields: FieldsArray;
+    signupData: SignupData;
+    setSignupData: (name: string, value: string) => void;
+    isValid: boolean;
+    fields: FieldsArray;
+}
 
-export type InputControlProps = {
-    subject: LoginInputSubject;
-    setCredentials: (inputName: string, inputValue: string) => void;
-    displayWarning: boolean;
-    handleWarnings: (warnFields: FieldsArray | null) => void;
-    subjectWarning: string;
+/**
+ * Auth related Types
+**/
+
+export type UserAuth = {
+    token: string;
+    expires: string;
+}
+export type User = {
+    name: string;
+    email: string;
 }
