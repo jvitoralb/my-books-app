92fa672a5c719d817e4d7fbb86bd38d67e657369
jvitoralb <jvvalb@gmail.com>
2023-10-10 22:45:25 -0300

    update: add authLoader to some pages

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -8,17 +8,17 @@ import Home from './pages/home/Home.tsx';
 import homeLoader from './pages/home/home.loader.ts';
 import Welcome from './pages/welcome/Welcome.tsx';
 import LogInPage from './pages/login/Login.tsx';
-import loginLoader from './pages/login/login.loader.ts';
 import SignUp from './pages/signup/Signup.tsx';
+import authLoader from './pages/loader/auth.ts';
 
 
 const browserAppRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index element={<Home />} loader={homeLoader} />
-      <Route path='/welcome' element={<Welcome />} />
-      <Route path='/login' element={<LogInPage />} loader={loginLoader} />
-      <Route path='/signup' element={<SignUp />} />
+      <Route path='/welcome' element={<Welcome />} loader={authLoader} />
+      <Route path='/login' element={<LogInPage />} loader={authLoader} />
+      <Route path='/signup' element={<SignUp />} loader={authLoader} />
     </Route>
   )
 );
