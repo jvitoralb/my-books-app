7cf9e5ff85879a8cec12770c9e34bab06659f2fb
jvitoralb <jvvalb@gmail.com>
2023-11-09 23:44:23 -0300

    update: pages names

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,21 +4,21 @@ import {
   Route
 } from 'react-router-dom';
 import App from './App.tsx';
-import Home from './pages/home/Home.tsx';
+import HomePage from './pages/home/Home.tsx';
 import homeLoader from './pages/home/home.loader.ts';
 import Welcome from './pages/welcome/Welcome.tsx';
 import LogInPage from './pages/login/Login.tsx';
-import SignUp from './pages/signup/Signup.tsx';
+import SignUpPage from './pages/signup/Signup.tsx';
 import authLoader from './pages/loader/auth.ts';
 
 
 const browserAppRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index element={<Home />} loader={homeLoader} />
+      <Route index element={<HomePage />} loader={homeLoader} />
       <Route path='/welcome' element={<Welcome />} loader={authLoader} />
       <Route path='/login' element={<LogInPage />} loader={authLoader} />
-      <Route path='/signup' element={<SignUp />} loader={authLoader} />
+      <Route path='/signup' element={<SignUpPage />} loader={authLoader} />
     </Route>
   )
 );
