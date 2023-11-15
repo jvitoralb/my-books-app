d0f8fbbb25ea404408ef2a99f15e267a75314ac9
jvitoralb <jvvalb@gmail.com>
2023-11-14 23:34:19 -0300

    update: import paths and add settings route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,19 +4,22 @@ import {
   Route
 } from 'react-router-dom';
 import App from './App.tsx';
-import HomePage from './pages/home/Home.tsx';
+import HomePage from './pages/home/HomePage.tsx';
 import homeLoader from './pages/home/home.loader.ts';
-import Welcome from './pages/welcome/Welcome.tsx';
-import LogInPage from './pages/login/Login.tsx';
-import SignUpPage from './pages/signup/Signup.tsx';
+import WelcomePage from './pages/welcome/WelcomePage.tsx';
+import LogInPage from './pages/login/LoginPage.tsx';
+import SignUpPage from './pages/signup/SignupPage.tsx';
 import authLoader from './pages/loader/auth.ts';
+import SettingsPage from './pages/settings/SettingsPage.tsx';
+import settingsLoader from './pages/settings/settingsLoader.ts';
 
 
 const browserAppRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index element={<HomePage />} loader={homeLoader} />
-      <Route path='/welcome' element={<Welcome />} loader={authLoader} />
+      <Route path='/settings' element={<SettingsPage />} loader={settingsLoader} />
+      <Route path='/welcome' element={<WelcomePage />} loader={authLoader} />
       <Route path='/login' element={<LogInPage />} loader={authLoader} />
       <Route path='/signup' element={<SignUpPage />} loader={authLoader} />
     </Route>
