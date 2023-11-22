1b8763d519bcdacf0ade35b6ab8c7804d1855528
jvitoralb <jvvalb@gmail.com>
2023-11-22 13:45:12 -0300

    update: treatLabels implementation

--- frontend/src/utils/strings.ts
+++ frontend/src/utils/strings.ts
@@ -2,17 +2,37 @@ export const firstCharToUpper = (word: string) => {
     return word.charAt(0).toLocaleUpperCase() + word.slice(1);
 }
 
+const replaceChar = (string: string, char: string, replaceChar: string) => {
+    let newStr = '';
+    
+    for(let i = 0; i < string.length; i++) {
+        if (string.charAt(i) === char) {
+            newStr += replaceChar;
+        } else {
+            newStr += string.charAt(i);
+        }
+    }
+
+    return newStr;
+}
+
 export const treatLabels = (label: string) => {
     let newLabel = '';
     
     if (label.includes('_')) {
-        for(let i = 0; i < label.length; i++) {
-            if (label[i] === '_') {
-                newLabel += ' ';
-            } else {
-                newLabel += label[i];
-            }
-        }
+        newLabel = replaceChar(label, '_', ' ');
+    } else {
+        newLabel = label;
+    }
+
+    return firstCharToUpper(newLabel);
+}
+
+export const treatLabelsHyphen = (label: string) => {
+    let newLabel = '';
+    
+    if (label.includes('-')) {
+        newLabel = replaceChar(label, '-', ' ');
     } else {
         newLabel = label;
     }
