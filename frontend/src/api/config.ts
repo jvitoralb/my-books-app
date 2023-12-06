99a0ccd236a8f60669b74df316603e64d3508760
jvitoralb <jvvalb@gmail.com>
2023-12-05 23:04:44 -0300

    upload: created axios config file

---
+++ frontend/src/api/config.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import handleAuth from '../utils/auth';
+
+const serverBaseUrl = import.meta.env.PROD ? import.meta.env.VITE_PROD_URL : import.meta.env.VITE_DEV_URL;
+
+const axiosInstance = axios.create({
+    baseURL: serverBaseUrl,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+const USER_LOGIN_URL = '/users/login';
+const USER_REGISTER_URL = '/users/register';
+const publicUrls: [string, string] = [USER_LOGIN_URL, USER_REGISTER_URL];
+const authToken = (handleAuth()).getToken();
+
+axiosInstance.interceptors.request.use(function(config) {
+    if (config.url && !publicUrls.includes(config.url)) {
+        config.headers['Authorization'] = authToken;
+    }
+    return config;
+});
+
+export default axiosInstance;
