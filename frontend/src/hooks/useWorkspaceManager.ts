c2849fbaa1025981c5753c2b78101f7319028b54
jvitoralb <jvvalb@gmail.com>
2023-12-22 18:04:11 -0300

    update: implement note storage handler

--- frontend/src/hooks/useWorkspaceManager.ts
+++ frontend/src/hooks/useWorkspaceManager.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { MutationStatus } from '@tanstack/react-query';
 import { BookNote } from '../types';
+import handleNoteStorage from '../utils/noteStorage';
 
 type WorkspaceManager = {
     notes: BookNote[];
@@ -11,11 +12,13 @@ type WorkspaceManager = {
 
 const useWorkspaceManager = ({ notes, createNoteStatus, newNote, deleteNoteStatus }: WorkspaceManager) => {
     const [note, setNote] = useState<BookNote | null>(null);
+    const storageHandler = handleNoteStorage();
 
     useEffect(() => {
         if (createNoteStatus === 'success' && newNote) {
             selectNote(newNote.id);
         } else if (deleteNoteStatus === 'success') {
+            storageHandler.deleteAllInfo(note!.id);
             resetNote();
         }
     }, [notes, createNoteStatus, deleteNoteStatus]);
@@ -24,6 +27,11 @@ const useWorkspaceManager = ({ notes, createNoteStatus, newNote, deleteNoteStatu
         for (let i = 0; i < notes.length; i++) {
             if (notes[i].id === id) {
                 setNote(notes[i]);
+                storageHandler.setAllInfo(notes[i].id, {
+                    title: notes[i].title,
+                    author: notes[i].author || '',
+                    about: notes[i].about,
+                });
                 break;
             }
         }
