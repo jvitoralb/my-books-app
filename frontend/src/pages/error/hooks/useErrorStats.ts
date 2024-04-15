6b027ac7e2ba6c9ff67ec056d031b36ab22fb45d
jvitoralb <jvvalb@gmail.com>
2024-04-15 15:04:25 -0300

    fix: user-friendly error messages

--- frontend/src/pages/error/hooks/useErrorStats.ts
+++ frontend/src/pages/error/hooks/useErrorStats.ts
@@ -1,19 +1,48 @@
 import { useEffect, useState } from 'react';
 import { isRouteErrorResponse } from 'react-router-dom';
 
+type CustomMessages = {
+    400: string;
+    404: string;
+    500: string;
+    isKey: (val: string | number) => val is keyof CustomMessages;
+    getMessage: (num: number) => string;
+}
+
 const useErrorStats = (error: unknown) => {
     const [ errorStats, setErrorStats ] = useState({
-        message: 'Something went wrong!',
-        statusCode: 500,
+        message: '',
+        statusCode: Number(),
     });
+
+    const customMessages: CustomMessages = {
+        400: 'This is a Bad Request...',
+        404: 'The page you\'re looking for doesn\'t exist...',
+        500: 'Something went wrong. Please try again later!',
+        isKey: function(val: string | number): val is keyof CustomMessages {
+            return val in customMessages;
+        },
+        getMessage: function(num: number): string {
+            if (this.isKey(num)) {
+                return this[num];
+            }
+            return this[500];
+        }
+    }
+
+    const setter = (statusCode: number) => {
+        setErrorStats((prev) => ({
+            ...prev,
+            statusCode,
+            message: customMessages.getMessage(statusCode),
+        }));
+    }
 
     useEffect(() => {
         if (isRouteErrorResponse(error)) {
-            setErrorStats((prev) => ({
-                ...prev,
-                statusCode: error.status,
-                message: error.data,
-            }));
+            setter(error.status);
+        } else {
+            setter(500);
         }
     }, []);
 
