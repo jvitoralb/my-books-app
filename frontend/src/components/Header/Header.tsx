2c2c49328dc6e86dbdec65d3895198907416c506
jvitoralb <jvvalb@gmail.com>
2024-04-28 12:46:50 -0300

    fix: update public header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,15 +2,35 @@ import { Heading } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 import './styles/header.css';
 
+type HeaderProps = {
+  location: string;
+}
 
-function Header() {
+function Header({ location }: HeaderProps) {
+  const isPublicRoute = ![
+    '/',
+    '/settings'
+  ].includes(location);
+
   return (
     <header id="my-books-app-header">
-      <Heading as="h1" size="md" fontWeight="semibold" textAlign="center">
+      <Heading 
+        as="h1" size="md" fontWeight="semibold" 
+        textAlign="center" py={isPublicRoute ? "0px" : "9px"}
+      >
         <NavLink to="/" >
-          my-books-app
+          mynotesapp
         </NavLink>
       </Heading>
+      {
+        isPublicRoute &&
+        <nav id="navigation-bar">
+          <ul>
+            <li><NavLink to="/login" className="nav-bar-link">Login</NavLink></li>
+            <li><NavLink to="/signup" className="nav-bar-link">Signup</NavLink></li>
+          </ul>
+        </nav>
+      }
     </header>
   );
 }
