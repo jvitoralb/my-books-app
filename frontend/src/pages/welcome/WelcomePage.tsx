496fe82e301ff523336c6a44e0d41d37b0b477bf
jvitoralb <jvvalb@gmail.com>
2024-04-28 11:55:07 -0300

    fix: implement welcome component

--- frontend/src/pages/welcome/WelcomePage.tsx
+++ frontend/src/pages/welcome/WelcomePage.tsx
@@ -1,23 +1,10 @@
-import reactLogo from '/assets/react.svg';
-import viteLogo from '/assets/vite.svg';
-import { Link } from 'react-router-dom';
+import Welcome from '../../components/Welcome';
 
-function Welcome() {
+
+function WelcomePage() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Welcome</h1>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
-    </>
+    <Welcome />
   );
 }
 
-export default Welcome;
+export default WelcomePage;
