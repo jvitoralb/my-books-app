5ce1b0892fed2d35627f58d063c32f45e62144a2
jvitoralb <jvvalb@gmail.com>
2024-04-28 12:47:58 -0300

    feat: use location to change hader and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,26 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import Header from './components/Header';
 import LoadingDOM from './components/LoadingDOM';
+import Footer from './components/Footer';
 
 
 function App() {
   const nav = useNavigation();
+  const location = useLocation();
 
   return (
     <>
-      <Header />
+      <Header
+        location={location.pathname}
+      />
       {
         nav.state === 'loading' ? 
         <LoadingDOM /> : 
         <Outlet />
       }
+      <Footer
+        location={location.pathname}
+      />
     </>
   );
 }
