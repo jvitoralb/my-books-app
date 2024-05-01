07b42c46ca932caa3ae49628d025412720321362
jvitoralb <jvvalb@gmail.com>
2024-05-01 13:45:10 -0300

    feat: create docs page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,11 +5,12 @@ import {
 } from 'react-router-dom';
 import App from './App.tsx';
 import HomePage from './pages/home/HomePage.tsx';
+import homeLoader from './pages/home/homeLoader.ts';
 import WelcomePage from './pages/welcome/WelcomePage.tsx';
+import DocsPage from './pages/docs/DocsPage.tsx';
 import LogInPage from './pages/login/LoginPage.tsx';
 import SignUpPage from './pages/signup/SignupPage.tsx';
 import SettingsPage from './pages/settings/SettingsPage.tsx';
-import homeLoader from './pages/home/homeLoader.ts';
 import settingsLoader from './pages/settings/settingsLoader.ts';
 import ErrorPage from './pages/error/ErrorPage.tsx';
 
@@ -20,6 +21,7 @@ const browserAppRouter = createBrowserRouter(
       <Route index element={<HomePage />} loader={homeLoader} />
       <Route path='/settings' element={<SettingsPage />} loader={settingsLoader} />
       <Route path='/welcome' element={<WelcomePage />} />
+      <Route path='/docs' element={<DocsPage />} />
       <Route path='/login' element={<LogInPage />} />
       <Route path='/signup' element={<SignUpPage />} />
     </Route>
