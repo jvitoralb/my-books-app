fe377bbe81d00fe6a9ee715cbb56cff90df03fc3
jvitoralb <jvvalb@gmail.com>
2024-05-09 20:43:30 -0300

    fix: removed signup and signin related tests

--- backend/src/components/user/tests/unit.spec.ts
+++ backend/src/components/user/tests/unit.spec.ts
@@ -5,33 +5,6 @@ import { BadRequestError } from '../../../lib/errors/custom';
 
 
 describe('User Validation Tests', () => {
-    test('throws a BadRequestError when user credentials are missing', () => {
-        let { req, res } = httpMocks.createMocks({
-            method: 'POST',
-            path: '/api/v1/users/login',
-            body: { email: 'user.test@library.app' }
-        }, {});
-        let next = () => {};
-
-        expect(() => new UserMiddleware().validateReadCredentials(req, res, next))
-        .toThrow(new BadRequestError('Missing required field'));
-    });
-
-    test('throws BadRequestError when is missing data for creating a user', () => {
-        let { req, res } = httpMocks.createMocks({
-            method: 'POST',
-            path: '/api/v1/users/register',
-            body: {
-                name: 'user test',
-                password: 'strongpswd123'
-            }
-        }, {});
-        let next = () => {};
-
-        expect(() => new UserMiddleware().validateCreate(req, res, next))
-        .toThrow(new BadRequestError('Missing required field'));
-    });
-
     test('throws BadRequestError when is missing data to update a user email', () => {
         let { req, res } = httpMocks.createMocks({
             method: 'PUT',
